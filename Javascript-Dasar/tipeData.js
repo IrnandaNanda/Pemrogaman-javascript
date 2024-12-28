db86a7a66f6df193aba68b3f79b5629212c3b207
IrnandaNanda <irnandatri1509@gmail.com>
2024-12-28 07:13:15 +0700

    tipedata

---
+++ Javascript-Dasar/tipeData.js
@@ -0,0 +1,31 @@
+console.log('=====Tipe Data=====');
+
+//Didalam JS tidak ada integer atau bilangan bulat
+console.log(999999999999999);
+console.log(9999999999999999);
+
+//Tipe data string
+console.log("Hello World");
+console.log('Hello World');
+
+//Tipe data boolean
+console.log(true);
+console.log(false);
+
+//Tipe data number
+console.log(123);
+
+//Tipe data null
+console.log(null);
+
+//Tipe data undefined
+console.log(undefined);
+
+//Tipe data symbol
+console.log(Symbol());
+
+//Tipe data object
+console.log({ name: 'John Doe' });
+
+//Tipe data function
+console.log(function () {});    
