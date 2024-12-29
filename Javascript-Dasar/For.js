cb9ae38b3564f7fb21902e6722b61f94aaddb79d
IrnandaNanda <irnandatri1509@gmail.com>
2024-12-29 09:20:15 +0700

    latihan for

--- Javascript-Dasar/For.js
+++ Javascript-Dasar/For.js
@@ -1,3 +1,21 @@
+
+//Pengualangan dengan For
 for (let nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++) {
-    console.log("Hello World " + nilaiAwal);
+    console.log("Hello World " + nilaiAwal + "x");
+}
+
+console.log("========================");
+
+//Latihan
+let jumlahAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik.");
+    noAngkot++;
+}
+
+for (let noAngkot = angkotBeroperasi + 1; noAngkot <= jumlahAngkot; noAngkot++) {
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.");
 }
