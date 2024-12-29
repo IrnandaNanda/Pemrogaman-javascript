5e773ed0334c6a1d064a5ace37d0727f2dd24e03
IrnandaNanda <irnandatri1509@gmail.com>
2024-12-29 08:47:30 +0700

    Pengulangan while

---
+++ Javascript-Dasar/Pengulangan(While).js
@@ -0,0 +1,17 @@
+console.log("=====Looping====");
+
+//While
+let i = 1;
+while (i <= 6) {
+    console.log("Hello World yang ke " + i);
+    i++;
+}
+console.log("========================");
+
+//Latihan
+
+let angkot = 1;
+while (angkot <= 10) {
+    console.log("Angkot No. " + angkot + " beroperasi dengan baik.");
+    angkot++;
+}
