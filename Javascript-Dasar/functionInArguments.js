87d0e541b0a0d9f6b3e9c82cc7ec0ae8209aee83
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-02 00:05:47 +0700

    function args

--- Javascript-Dasar/functionInArguments.js
+++ Javascript-Dasar/functionInArguments.js
@@ -12,3 +12,13 @@ console.log(kali(2,3));
 
 //Function In Argument
 console.log(kali(tambah(2,3), tambah(2,3)))
+
+function tambahBaru() {
+    let hasil = 0;
+    for (i=0; i<arguments.length; i++) {
+        hasil += arguments[i]
+    }
+    return hasil;
+}
+let coba = tambahBaru(1,2,3);
+console.log(coba)
