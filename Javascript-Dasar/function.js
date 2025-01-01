4fc3935279ab9c7ee658bae8329f5d4675ecb2b2
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-01 22:53:03 +0700

    Latihan Function

--- Javascript-Dasar/function.js
+++ Javascript-Dasar/function.js
@@ -6,3 +6,41 @@ function PenjumlahanVolume(a, b) {
     return c
 }
 console.log(PenjumlahanVolume(8,3));
+
+//Latihan 
+function addNumbers(a, b) {
+
+    let c;
+
+    c = a + b;
+
+    return c;
+}
+
+console.log(addNumbers(3,7))
+
+//Latihan2
+function isEven(num) {
+
+    if (num % 2 == 0) {
+        console.log ("Genap")
+    } else {
+        console.log("Ganjil")
+    }
+
+    return num;
+
+}
+
+console.log(isEven(9));
+
+//Latihan3
+function calculateSquareArea(side) {
+    let luas;
+
+    luas = side * side ;
+
+    return luas;
+}
+
+console.log(calculateSquareArea(5))
