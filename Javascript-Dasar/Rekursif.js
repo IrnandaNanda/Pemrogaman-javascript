dcf17f5b85f0a8ce3b86b024958baa2fabfa15ec
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-02 22:37:15 +0700

    Penyederhanaah dengan rekurdif

--- Javascript-Dasar/Rekursif.js
+++ Javascript-Dasar/Rekursif.js
@@ -1,6 +1,17 @@
+//Menggunakan Rekursif
 function faktorial(n) {
     if ( n === 0 ) return 1;
     return n * faktorial(n - 1);
 }
 
+
+//Menggunakan Looping
+function faktorialLooping(n) {
+    let hasil = 1;
+for (i=n; i > 0; i--) {
+        hasil *= i;
+    } return hasil;
+}
+
 console.log(faktorial(5))
+console.log(faktorialLooping(5))
