e786e6b31490ebc14b2ba5166a687ae823872a98
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-04 09:52:09 +0700

    Method Array

--- Javascript-Dasar/manipulationArray.js
+++ Javascript-Dasar/manipulationArray.js
@@ -16,3 +16,23 @@ let arr3 = ['Irnanda', 'Rexi', "Agung", 'Bowo']
 for (i=0; i < arr3.length; i++) {
     console.log('Mahasiswa ke ' + (i+1) + ': '+ arr3[i]);
 }
+
+let arrMethod = ['Irnanda', 'Putri', 'Haji']
+
+//1. Join
+console.log(arrMethod.join (' - ')) // Untung menggabungkan isi array
+
+//2. Push & Pop
+arrMethod.push('Agus', 'Sedih'); // Untuk Menambahkan array di index yang paling terakhir
+console.log(arrMethod.join (' - '));
+
+arrMethod.pop() // Untuk menghapus Array index paling terakhir
+console.log(arrMethod.join (' - '));
+
+//3. Unshift & Shift
+arrMethod.unshift('S.kom') // Untuk menambahkan Array pada index pertama
+console.log(arrMethod.join (' - '));
+
+arrMethod.shift() // Untuk menghapus Array index pertama
+console.log(arrMethod.join (' - '));
+
