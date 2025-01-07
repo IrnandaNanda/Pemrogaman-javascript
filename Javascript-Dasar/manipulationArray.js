481f1d187b2a030afa12744bafce6970a8d4170a
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-07 09:13:36 +0700

    manipulasi array

--- Javascript-Dasar/manipulationArray.js
+++ Javascript-Dasar/manipulationArray.js
@@ -36,3 +36,30 @@ console.log(arrMethod.join (' - '));
 arrMethod.shift() // Untuk menghapus Array index pertama
 console.log(arrMethod.join (' - '));
 
+console.log('========================================')
+
+//4. Splice & Slice
+let arrMethod2 = ['Anggur', 'Apel', 'Pisang']
+
+arrMethod2.splice(1,0,'Semangka') //Untuk menambahkan elemen di tengah, tidak dari awal ataupun akhir
+console.log(arrMethod2.join (' - '));
+
+let arrSlice = arrMethod2.slice(1,3) //Mengambil Array dari index ke 1 sampai index ke 2
+console.log(arrSlice.join (' - '));
+
+console.log('=================================================')
+
+let arrAngka = [1,2,3,4,5,6,7]
+// for (i = 0; i < arrAngka.length; i++) {
+//     // console.log(arrAngka.join (' - '));
+//     console.log(arrAngka[i])   
+// }
+
+
+//For Each tidak mengembalikan array melainkan mengembalikan String/Number
+arrAngka.forEach(function(e) {
+    console.log(e)
+})
+
+
+
