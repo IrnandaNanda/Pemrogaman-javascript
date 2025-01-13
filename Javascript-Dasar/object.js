ebfd359d88950754beed322885395c15b33cc793
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-13 09:19:40 +0700

    object

---
+++ Javascript-Dasar/object.js
@@ -0,0 +1,17 @@
+const orang = {
+    nama : 'Irnanda',
+    alamat : 'Indonesia',
+    umur : 21
+}
+
+//Menambah atau mengubah object
+// orang["nama"] = "Irnanda"
+// orang["alamat"] = "Indonesia"
+// orang["umur"] = 20
+
+console.table(orang)
+
+//Untuk menampilkan isi dari salah satu object
+console.info(`Nama : ${orang.nama}`)
+console.info(`Alamat : ${orang.alamat}`)
+console.info(`Umur : ${orang.umur}`)
