4bacc93a81da2a117edf9a90ef3be85d6ef2d9e7
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-23 21:51:42 +0700

    built in function

---
+++ Javascript-Dasar/Built-In-Function.js
@@ -0,0 +1,19 @@
+let myAction = "I am learning Javascript";
+
+console.log(myAction)
+console.log(myAction.length);
+console.log(myAction.toUpperCase());
+console.log(myAction.toLowerCase());
+
+
+// Split : Memecah String Besar Mnejadi String yang lebih kecil Menggunakan pola tertentu, contoh tanda spasi (" ")
+console.log(myAction.split(" "))
+
+// Substring : Mengambil Sebagian string saja berdasarkan posisi
+console.log(myAction.substring(0, 4))
+
+// random : Membuat Sebuah angka acak menggunakan Javascript
+console.log(Math.random())
+
+// round : Pembulatan angka
+console.log(Math.round(Math.random() * 10))
