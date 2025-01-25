d273edd26b52dfc07fd6c3094f55af9ef27d6845
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-25 23:51:11 +0700

    Array

---
+++ Javascript-Dasar/Array.js
@@ -0,0 +1,31 @@
+// Contoh Implementasi Array
+
+let myList = []; //Array Kosong
+myList = ["ini", "Adalah", "Array"] //Mengisi Array
+console.table(myList); //Menampilan array dengan index
+
+// Cara Memanggil Array
+let fruits = ["Apple", "Orange", "Manggo"];
+console.table(fruits)
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits.length) // Mencari Panjang Suatu array
+
+// Menambahkan & Menghapus Array elemen/Item terakhir
+let theDay = ["Senin", "Selasa", "Rabu"];
+console.table(theDay)
+
+theDay.push("Kamis") //menambahkan array elemen terakhir
+console.table(theDay)
+
+theDay.pop() // Menghapus Array elemen terakhir
+console.table(theDay)
+
+theDay.unshift("Minggu") // Menambahkan Array Elemen pertama
+console.table(theDay)
+
+theDay.shift() // Menghapus Array Elemen Pertama
+console.table(theDay)
+
+
---
+++ Javascript-Dasar/Exercise-Modular.js
@@ -0,0 +1,36 @@
+function kelvinToCelsius(kelvin) {
+    // your code here
+  }
+  
+  function kelvinToFahrenheit(kelvin) {
+    // your code here
+  }
+  
+  function celsiusToFahrenheit(celsius) {
+    // your code here
+  }
+  
+  function celsiusToKelvin(celsius) {
+    // your code here
+  }
+  
+  function fahrenheitToKelvin(fahrenheit) {
+    // your code here
+  }
+  
+  function fahrenheitToCelsius(fahrenheit) {
+    // your code here
+  }
+  
+  function convertTemperature(temperature, initialUnit, finalUnit) {
+    // your code here
+  }
+  
+  console.log(convertTemperature(0, 'C', 'K')); // 273.15
+  console.log(convertTemperature(0, 'C', 'F')); // 32
+  
+  console.log(convertTemperature(0, 'F', 'C')); // -17.78
+  console.log(convertTemperature(0, 'F', 'K')); // 255.37
+  
+  console.log(convertTemperature(0, 'K', 'C')); // -273.15
+  console.log(convertTemperature(0, 'K', 'F')); // -459.67
