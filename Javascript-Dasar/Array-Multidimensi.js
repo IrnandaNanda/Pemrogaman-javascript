391c60ecd7c3af69c963825df054612c9d577fa4
IrnandaNanda <irnandatri1509@gmail.com>
2025-01-26 21:35:51 +0700

    Array Multidimensi

---
+++ Javascript-Dasar/Array-Multidimensi.js
@@ -0,0 +1,17 @@
+// Multidimensi Array
+const data = [
+    [1,2,3],
+    [3,4,1],
+    [4,5,6],
+];
+console.table(data)
+
+let activities = [
+    ["study", 9],
+    ["Eat", 1],
+    ["Play Game", 2],
+    ["Sleep", 8],
+];
+console.table(activities)
+console.log(activities[0])
+console.log(activities[0][0])
