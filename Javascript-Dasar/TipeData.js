b3d9a7be49b0a528dabe49f38d573db71dd536cf
IrnandaNanda <irnandatri1509@gmail.com>
2026-07-27 02:35:54 +0700

    TipeData

---
+++ Javascript-Dasar/TipeData.js
@@ -0,0 +1,17 @@
+// 1. Tipe Data String
+const currentYear = new Date().getFullYear();
+const text = `Sekarang adalah tahun ${currentYear}.`;
+
+console.log(text);
+
+// 2. Tipe Data Number
+const result = 50 / 0;
+console.log(result); // output: Infinity
+
+// 3. Tipe Data Boolean
+const completed = true;
+const passed = false;
+console.log(completed, passed);
+
+const isGreater = 5 > 2;
+console.log(isGreater)
